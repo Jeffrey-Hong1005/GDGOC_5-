c77a4fd1265ea0ae4d71f18e931893c1c3f54e53
Hong Junsoo <powerofjun@naver.com>
2025-10-04 17:06:52 +0900

    2주차

--- Spring/1주차/spring-study/src/main/java/com/gdg/spring_study/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-// package com.gdg.spring_study.repository.PostRepository.java
-
-package com.gdg.spring_study.repository;
-
-import com.gdg.spring_study.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
